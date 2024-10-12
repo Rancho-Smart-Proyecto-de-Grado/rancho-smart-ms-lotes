8731cd25a047eaa701b56091611c9e572a935476
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:29:36 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-lotes/src/main/java/com/rancho_smart/ms_lotes/MsLotesApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_lotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLotesApplication.class, args);
+	}
+
+}
---
+++ ms-lotes/src/test/java/com/rancho_smart/ms_lotes/MsLotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_lotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
